9a05e1047c41e3ee8eec9c956f69306c5ceadb31
Raul Jimenez <rjbarco@gmail.com>
2022-05-27 11:15:35 +0200

    Books are listed in a table and there is a New Book button with no functionality yet

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Books Constructor
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -11,5 +12,32 @@ function Book(title, author, pages, read) {
         }
     }
 }
-const theHobbit = new Book('The Hobbit','J.R.R. Tolkien',295,true);
-console.log(theHobbit.info());
+
+// Function that takes a book and place it into the Array
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+// Function that print the info of all the books in the library
+function listBooksInLibrary(library) {
+    const table = document.querySelector('#booksTable');
+    for (const i in library) {
+        if (Object.hasOwnProperty.call(library, i)) {
+            const element = library[i];
+            const bookFile = document.createElement('tr');
+            const bookData = document.createElement('td');
+            bookData.textContent = element.info();
+            bookFile.appendChild(bookData);
+            table.appendChild(bookFile);
+        }
+    }
+}
+
+let myLibrary = [];
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 256, true);
+const ikigaiForTeens = new Book("Ikigai for Teens: Finding Your Reason for Being", "Hector Garcia Puigcerver", 176, false);
+const vestidoDeNovia = new Book("Vestido de novia", "Pierre Lemaitre", 323, false);
+addBookToLibrary(theHobbit);
+addBookToLibrary(ikigaiForTeens);
+addBookToLibrary(vestidoDeNovia);
+listBooksInLibrary(myLibrary);
